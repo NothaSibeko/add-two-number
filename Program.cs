1a1545f446c2e3303669290b2d6d0e3c634fd5d0
notha <notha@LAPTOP-N9HV6Q5U>
2022-09-28 19:43:07 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace add
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNum1 = 20;
+            int myNum2 = 30;
+            int Sum = (myNum1 + myNum2);
+            Console.WriteLine(Sum);
+        }
+    }
+}
